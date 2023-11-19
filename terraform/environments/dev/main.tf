305f6b7f5234eab2814a50ebbfc07fc89338c6ae
Emmanuel C <eechukwu@gmail.com>
2023-11-19 20:22:38 +0000

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,3 +4,9 @@ module "compute_instance" {
   machine_type  = "e2-micro"      // Or any other type
   zone          = "us-central1-a" // Or any other zone
 }
+
+provider "google" {
+  project = "gcp-devops-pro-405617"
+  region  = "us-central1-a"  # Replace with your GCP region
+  # Optionally specify the zone
+}
