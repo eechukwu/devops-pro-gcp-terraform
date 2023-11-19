c286a91ea48c96182d504567f2a4a4c130acb854
Emmanuel C <eechukwu@gmail.com>
2023-11-19 18:44:57 +0000

    Update main.tf

--- terraform/modules/compute_instance/main.tf
+++ terraform/modules/compute_instance/main.tf
@@ -1,1 +1,18 @@
-compute_instance
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
