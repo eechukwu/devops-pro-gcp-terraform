5814b9439fe40d0ac2cad5acf080a04cd7c360a2
Emmanuel C <eechukwu@gmail.com>
2023-11-19 21:23:46 +0000

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,9 +1,9 @@
-module "compute_instance" {
+/* module "compute_instance" {
   source        = "../../modules/compute_instance"
   instance_name = "test-instance"
   machine_type  = "e2-micro"      // Or any other type
   zone          = "us-central1-a" // Or any other zone
-}
+}*/
 
 provider "google" {
   project = "gcp-devops-pro-405617"
