9315b213a6b658b9b4e47d8e63df72999acf8a89
Emmanuel C <eechukwu@gmail.com>
2023-11-19 21:40:29 +0000

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,12 +1,20 @@
-/* module "compute_instance" {
+terraform {
+  backend "gcs" {
+    bucket = "devops-pro-gcp-terraform-2023"  # Replace with the name of your GCS bucket
+    prefix = "terraform/state"
+  }
+}
+
+module "compute_instance" {
   source        = "../../modules/compute_instance"
   instance_name = "test-instance"
   machine_type  = "e2-micro"      // Or any other type
   zone          = "us-central1-a" // Or any other zone
-}*/
+}
 
 provider "google" {
   project = "gcp-devops-pro-405617"
   region  = "us-central1-a"  # Replace with your GCP region
   # Optionally specify the zone
 }
+
