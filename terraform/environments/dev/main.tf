d08f65c65fa91adda381b3403ee2bc59167f0137
Emmanuel C <eechukwu@gmail.com>
2023-12-24 17:41:32 +0000

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -27,6 +27,40 @@ module "instance_template" {
   access_config   = [local.access_config]
 }
 
+module "test-vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 8.0"
+  project_id   = var.project_id # Replace this with your project ID in quotes
+  network_name = "my-custom-mode-network"
+  mtu          = 1460
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-west1"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-west1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+    },
+    {
+      subnet_name               = "subnet-03"
+      subnet_ip                 = "10.10.30.0/24"
+      subnet_region             = "us-west1"
+      subnet_flow_logs          = "true"
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
+      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+      subnet_flow_logs_filter   = "false"
+    }
+  ]
+}
+
+
 provider "google" {
   project = "gcp-devops-pro-405617"
   region  = "europe-west2" # Replace with your GCP region
